9c5b1e261bbadf7e2cc291a6b73f5028eb4f6374
tmoi29 <tmoi@stuy.edu>
2018-03-26 05:03:01 -0400

    done

--- index.js
+++ index.js
@@ -8,6 +8,10 @@
 //Data = [Revenue (Trillion), Spending (Trillion), Deficit (Hundreds of Billions), Deficit % of GDP, GDP (Trillion)]
 var early_data = [2.6, 3.24, 6.37, 4.1, 15.5]; //2013 Obama
 var late_data = [3.21, 3.65, 4.43, 2.6, 17]; //2017 Drumpf
+var titles = ["Revenue (Trillion)", "Spending (Trillions)", "Deficit (Hundreds of Billions)", "Deficit % of GDP", "GDP (Trillions)"];
+
+var early_title = "Obama Llama's Stats"
+var late_title = "Duck Drumpf's Stats"
 
 
 var chart = d3.select(".chart");
@@ -23,18 +27,21 @@ var barEnter = function(update){
     return update.enter().append("div");
 };
 
-var barText = function(enter){
-    enter.text(function(d){return d;});
+var barText = function(enter, titles){
+    enter.text(function(d, i){return titles[i] + "  " + d;});
 };
 
 var anim = function(enter){
     enter.transition().duration(5000).style("width", function(d) {
-  return d * 20 + "px"; });
+  return d * 55 + "px"; });
 };
 
 var clear = function(){
-    while (chart.firstChild) {
-        chart.removeChild(chart.firstChild);
+    var c = document.getElementsByClassName("chart");
+    c = c[0];
+    document.getElementsByTagName("h2")[0].innerHTML = "";
+    while (c.firstChild) {
+        c.removeChild(c.firstChild);
     }
 };
 
@@ -42,16 +49,18 @@ var display_early = function(){
     clear();
     var update = barUpdate(early_data);
     var enter = barEnter(update);
-    barText(enter);
+    barText(enter, titles);
     anim(enter);
+    document.getElementsByTagName("h2")[0].innerHTML = early_title;
 };
 
 var display_late = function(){
     clear();
     var update = barUpdate(late_data);
     var enter = barEnter(update);
-    barText(enter);
+    barText(enter, titles);
     anim(enter);
+    document.getElementsByTagName("h2")[0].innerHTML = late_title;
 };
 
 early_btn.addEventListener("click", display_early);
